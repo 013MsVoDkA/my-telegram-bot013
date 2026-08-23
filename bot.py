b4ea45466d973e14a48699402de973438f3efed8
013MsVoDkA <milkuwu31@gmail.com>
2026-08-24 00:14:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ from telegram.ext import (
 TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
 OPENROUTER_API_KEY = os.environ.get("OPENROUTER_API_KEY")
 GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
-PORT = int(os.environ.get("PORT", 8080))
+PORT = int(os.environ.get("PORT", 10000))
 
 # Твой публичный URL на Render
 RENDER_EXTERNAL_URL = os.environ.get("RENDER_EXTERNAL_URL", "https://my-telegram-bot01322.onrender.com")
