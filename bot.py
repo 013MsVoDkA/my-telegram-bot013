f66615855da6bcd3435a52c8888f28243a15cb90
013MsVoDkA <milkuwu31@gmail.com>
2026-08-25 20:45:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -296,7 +296,14 @@ async def main():
 
     await app.initialize()
     await app.start()
-    app.updater.start_polling(allowed_updates=["message", "business_message", "business_connection", "edited_business_message"])
+await app.updater.start_polling(
+    allowed_updates=[
+        "message",
+        "business_message",
+        "business_connection",
+        "edited_business_message",
+    ]
+)
     
     stop_event = asyncio.Event()
     await stop_event.wait()
